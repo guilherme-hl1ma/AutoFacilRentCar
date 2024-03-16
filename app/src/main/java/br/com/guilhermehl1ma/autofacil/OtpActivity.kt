182dbc3db3ddbfc985ddaa5f32664f5f1b787479
guilherme-hl1ma <guihlima@outlook.com>
2024-03-16 12:44:43 -0300

    feat: creating account verifying with sms

--- app/src/main/java/br/com/guilhermehl1ma/autofacil/OtpActivity.kt
+++ app/src/main/java/br/com/guilhermehl1ma/autofacil/OtpActivity.kt
@@ -7,16 +7,18 @@ import android.util.Log
 import android.widget.Toast
 import br.com.guilhermehl1ma.autofacil.databinding.ActivityOtpScreenBinding
 import com.google.firebase.Firebase
-import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.auth.auth
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.functions
 
 class OtpActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOtpScreenBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var functions: FirebaseFunctions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +26,7 @@ class OtpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = Firebase.auth
+        functions = Firebase.functions("southamerica-east1")
 
         // get storedVerificationId from the intent
         val storedVerificationId = intent.getStringExtra("storedVerificationId")
@@ -35,7 +38,6 @@ class OtpActivity : AppCompatActivity() {
                 val credential : PhoneAuthCredential = PhoneAuthProvider.getCredential(
                     storedVerificationId.toString(), otp)
                 linkWithCredential(credential)
-
             }else{
                 Toast.makeText(this,"Enter OTP", Toast.LENGTH_SHORT).show()
             }
@@ -63,6 +65,7 @@ class OtpActivity : AppCompatActivity() {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+                    setSMSVerified(credential)
                     val intent = Intent(this , MainActivity::class.java)
                     startActivity(intent)
                     finish()
@@ -75,5 +78,11 @@ class OtpActivity : AppCompatActivity() {
                     }
                 }
             }
+    }
+
+    private fun setSMSVerified(phoneCredential: PhoneAuthCredential) {
+        val user = Firebase.auth.currentUser
+
+        user!!.updatePhoneNumber(phoneCredential)
     }
 }
